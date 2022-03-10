c33286f5b5a5cfe1ff320c881e72c813aac7a4d0
Angel Aguirre <angelaguirrel@hotmail.com>
2022-03-09 20:41:19 -0600

    Integración del proxy y definición de las reglas del getter

--- js/framework.js
+++ js/framework.js
@@ -1,11 +1,21 @@
 class Reactive {
     constructor(options) {
         this.origin = options.data();
+        this.$data = new Proxy(this.origin, {
+            get(target, name) {
+                if ( name in target ) {
+                    return target[name];
+                }
+
+                console.warn(`${name} is not defined in data`);
+                return '';
+            },
+        });
     }
 
     mount() {
         document.querySelectorAll('*[x-text]').forEach(el => {
-            this.xText(el, this.origin, el.getAttribute('x-text'));
+            this.xText(el, this.$data, el.getAttribute('x-text'));
         })
     }
 
