2c7dcb9ae6bee44e0ad4379109957e9772c62a0f
Angel Aguirre <angelaguirrel@hotmail.com>
2022-03-09 21:11:25 -0600

    Uso de Reflect para modificar propiedades, funcionalidad del setter

--- js/framework.js
+++ js/framework.js
@@ -3,12 +3,15 @@ class Reactive {
         this.origin = options.data();
         this.$data = new Proxy(this.origin, {
             get(target, name) {
-                if ( name in target ) {
-                    return target[name];
+                if ( Reflect.has(target, name) ) {
+                    return Reflect.get(target, name);
                 }
 
                 console.warn(`${name} is not defined in data`);
                 return '';
+            },
+            set(target, name, value) {
+                Reflect.set(target, name, value);
             },
         });
     }
@@ -16,14 +19,25 @@ class Reactive {
     mount() {
         document.querySelectorAll('*[x-text]').forEach(el => {
             this.xText(el, this.$data, el.getAttribute('x-text'));
-        })
+        });
+
+        document.querySelectorAll('*[x-model]').forEach(el => {
+            const name = el.getAttribute('x-model');
+            this.xModel(el, this.$data, name);
+
+            el.addEventListener('input', () => {
+                Reflect.set(this.$data, name, el.value);
+            });
+        });
     }
 
     xText(el, target, name) {
-        el.textContent = target[name];
+        el.textContent = Reflect.get(target, name);
     }
 
-    xModel() {}
+    xModel(el, target, name) {
+        el.value = Reflect.get(target, name);
+    }
 }
 
 function createApp(options) {
